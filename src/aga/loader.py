be3a019e7328ff7c2208f5814d4322bccb72505c
Riley Shahar <riley.shahar@gmail.com>
2021-09-25 12:51:33 -0700

    Add tools for loading student submissions

---
+++ src/aga/loader.py
@@ -0,0 +1,80 @@
+"""Tools for loading student-submitted, maybe-invalid code from source files."""
+
+import importlib.util
+import os
+from os.path import join as pathjoin
+from types import ModuleType
+from typing import Any
+
+
+class InvalidSubmissionError(BaseException):
+    """Something about the submission was invalid."""
+
+
+class TooManyMatchingSymbols(InvalidSubmissionError):
+    """Too many maching symbols were found."""
+
+
+class NoMatchingSymbol(InvalidSubmissionError, AttributeError):
+    """An expected symbol was not found."""
+
+
+class SubmissionSyntaxError(InvalidSubmissionError, SyntaxError):
+    """The submission held an invalid syntax."""
+
+
+def _load_source_from_path(path: str, name: str = "module") -> Any:
+    """Load the python source file found at path, absolute or relative, as a module.
+
+    There's a lot of weird stuff going on in this method with type signatures and
+    poorly-documented code that python uses internally for their `import` statement. I
+    got this implementation from https://stackoverflow.com/a/67692 and made only small
+    modifications to it, but I'm not 100% sure I can explain how it works.
+    """
+    spec = importlib.util.spec_from_file_location(name, path)
+
+    if spec is None:
+        # Based on inspection of the source, I'm not certain how this can happen, but my
+        # type checker insists it can. This seems like the most reasonable error to
+        # raise.
+        raise FileNotFoundError
+
+    mod = importlib.util.module_from_spec(spec)
+    try:
+        spec.loader.exec_module(mod)  # type: ignore
+    except (SyntaxError, NameError) as err:
+        raise SubmissionSyntaxError from err  # group all parse errors
+
+    return mod
+
+
+def _load_attr_from_module(attr: str, module: ModuleType) -> Any:
+    """Get a specific symbol from a module."""
+    try:
+        return module.__getattribute__(attr)
+    except AttributeError as err:
+        raise NoMatchingSymbol from err
+
+
+def load_symbol_from_path(path: str, symbol: str) -> Any:
+    """Load a specific symbol from a source file found at path, absolute or relative."""
+    mod = _load_source_from_path(path)
+    return _load_attr_from_module(symbol, mod)
+
+
+def load_symbol_from_dir(path: str, symbol: str) -> Any:
+    """Load a specific symbol from any of the source files in a directory."""
+    matching_symbols = []
+    for file in os.listdir(path):
+
+        try:
+            file_path = pathjoin(path, file)
+            matching_symbols.append(load_symbol_from_path(file_path, symbol))
+        except (FileNotFoundError, AttributeError, SyntaxError):
+            continue
+
+    if len(matching_symbols) > 1:
+        raise TooManyMatchingSymbols
+    if len(matching_symbols) == 0:
+        raise NoMatchingSymbol
+    return matching_symbols[0]
---
+++ tests/test_loader.py
@@ -0,0 +1,148 @@
+"""Tests for the `loader` module."""
+
+from pathlib import Path
+from typing import Iterable
+
+import pytest
+
+from aga.loader import (
+    NoMatchingSymbol,
+    SubmissionSyntaxError,
+    TooManyMatchingSymbols,
+    load_symbol_from_dir,
+    load_symbol_from_path,
+)
+
+SOURCE_SQUARE = """
+def square(x: int) -> int:
+    return x * x
+"""
+
+SOURCE_DUPLICATE = """
+def duplicate(x: int):
+    return (x, x)
+"""
+
+SOURCE_CAR = """
+class Car:
+    def __init__(self):
+        self._distance = 0
+
+    def drive(self, distance: int):
+        self._distance += distance
+
+    def distance(self) -> int:
+        return self._distance
+"""
+
+SOURCE_INVALID = """
+This is not valid python code!
+"""
+
+
+def _write_source_to_file(path: Path, source: str) -> str:
+    """Write source code to a file, returning a string of its path."""
+    with open(path, "w", encoding="UTF-8") as file:
+        file.write(source)
+
+    return str(path)
+
+
+def _write_sources_to_files(
+    path: Path, sources: Iterable[str], filenames: Iterable[str]
+) -> str:
+    """Write a series of source files to files in path, returning the directory path."""
+    for source, file in zip(sources, filenames):
+        _write_source_to_file(path.joinpath(file), source)
+
+    return str(path)
+
+
+@pytest.fixture(name="source_square")
+def fixture_source_square(tmp_path: Path) -> str:
+    """Generate a source file with SOURCE_SQUARE, returning its path."""
+    return _write_source_to_file(tmp_path.joinpath("src.py"), SOURCE_SQUARE)
+
+
+@pytest.fixture(name="source_dir")
+def fixture_source_dir(tmp_path: Path) -> str:
+    """Generate a directory containing numerous valid and invalid source files.
+
+    The directory contains:
+    - invalid.txt, an invalid python file.
+    - square.py, which contains a `square` function.
+    - car.py, which contains a `Car` class and may be tested by `car_tester`.
+    - duplicate-one.py, which contains a `duplicate` function.
+    - duplicate-two.py, which contains a `duplicate` function.
+    """
+    return _write_sources_to_files(
+        tmp_path,
+        (SOURCE_CAR, SOURCE_INVALID, SOURCE_SQUARE, SOURCE_DUPLICATE, SOURCE_DUPLICATE),
+        ("car.py", "invalid.txt", "square.py", "duplicate-one.py", "duplicate-two.py"),
+    )
+
+
+def car_tester(Car: type) -> None:  # pylint: disable=invalid-name
+    """Test an imported Car class."""
+    car = Car()
+    car.drive(50)
+    assert car.distance() == 50
+
+
+def test_load_symbol_from_path_square(source_square: str) -> None:
+    """Test that load_symbol_from_path loads a function."""
+
+    square = load_symbol_from_path(source_square, "square")
+    assert square(5) == 25
+
+
+def test_load_symbol_from_path_car(tmp_path: Path) -> None:
+    """Test that load_symbol_from_path loads a class."""
+    path = _write_source_to_file(tmp_path.joinpath("src.py"), SOURCE_CAR)
+
+    Car = load_symbol_from_path(path, "Car")  # pylint: disable=invalid-name
+    car_tester(Car)
+
+
+def test_load_symbol_from_path_invalid_source_errors(tmp_path: Path) -> None:
+    """Test that load_symbol_from_path errors on invalid source."""
+    path = _write_source_to_file(tmp_path.joinpath("src.py"), SOURCE_INVALID)
+
+    with pytest.raises(SubmissionSyntaxError):
+        load_symbol_from_path(path, "foo")
+
+
+def test_load_symbol_from_path_no_file_errors(tmp_path: Path) -> None:
+    """Test that load_symbol_from_path errors on nonexistent files."""
+    with pytest.raises(FileNotFoundError):
+        load_symbol_from_path(str(tmp_path.joinpath("src.py")), "foo")
+
+
+def test_load_symbol_from_path_no_symbol_errors(source_square: str) -> None:
+    """Test that load_symbol_from_path errors on nonexistent symbols."""
+    with pytest.raises(NoMatchingSymbol):
+        load_symbol_from_path(source_square, "foo")
+
+
+def test_load_symbol_from_dir_function(source_dir: str) -> None:
+    """Test that load_symbol_from_dir loads a function."""
+    square = load_symbol_from_dir(source_dir, "square")
+    assert square(5) == 25
+
+
+def test_load_symbol_from_dir_class(source_dir: str) -> None:
+    """Test that load_symbol_from_dir loads a class."""
+    Car = load_symbol_from_dir(source_dir, "Car")  # pylint: disable=invalid-name
+    car_tester(Car)
+
+
+def test_load_symbol_from_dir_nonexistent_symbol_errors(source_dir: str) -> None:
+    """Test that load_symbol_from_dir errors on nonexistent symbols."""
+    with pytest.raises(NoMatchingSymbol):
+        load_symbol_from_dir(source_dir, "foo")
+
+
+def test_load_symbol_from_dir_multiple_symble_errors(source_dir: str) -> None:
+    """Test that load_symbol_from_dir errors on nonexistent symbols."""
+    with pytest.raises(TooManyMatchingSymbols):
+        load_symbol_from_dir(source_dir, "duplicate")
