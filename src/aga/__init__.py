4e32ecbdc955e94605186713e8df0c212391e8ee
Riley Shahar <riley.shahar@gmail.com>
2021-09-28 00:37:24 -0700

    Rewrite readme

--- src/aga/__init__.py
+++ src/aga/__init__.py
@@ -1,4 +1,8 @@
-"""Aga grades assignments: a library for easily producing autograders for code."""
+"""Aga grades assignments: a library for easily producing autograders for code.
+
+Anything not explicitly documented here should not be used directly by clients and is
+only exposed for testing, the CLI, and type hinting.
+"""
 
 from .core import problem, test_case
 
