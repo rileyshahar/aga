7151b7afee495a4ba797c9b9680d9ad27a5295bc
Riley Shahar <riley.shahar@gmail.com>
2021-09-27 23:17:53 -0700

    Fix formatting
    Make lints happy!

--- src/aga/__init__.py
+++ src/aga/__init__.py
@@ -1,5 +1,5 @@
 """Aga grades assignments: a library for easily producing autograders for code."""
 
-from .core import  problem, test_case
+from .core import problem, test_case
 
 __all__ = ("problem", "test_case")
