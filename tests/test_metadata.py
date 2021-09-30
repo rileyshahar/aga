7640f165aea6786555a5999703696e551d68e85e
Riley Shahar <riley.shahar@gmail.com>
2021-09-29 23:38:13 -0700

    Single-source the version

---
+++ tests/test_metadata.py
@@ -0,0 +1,8 @@
+"""Test the package metadata."""
+
+from aga import __version__ as agaversion
+
+
+def test_version() -> None:
+    """Test that the version is correct."""
+    assert agaversion == "0.3.2"
