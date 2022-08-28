8c3a9096bcec01ff6f4d7f69450e38dbca877028
Riley Shahar <riley.shahar@gmail.com>
2022-08-27 19:09:42 -0700

    Bump minor version

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -5,4 +5,4 @@ from aga import __version__ as agaversion
 
 def test_version() -> None:
     """Test that the version is correct."""
-    assert agaversion == "0.7.1"
+    assert agaversion == "0.8.1"
