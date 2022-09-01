318b1415ff2523663c2474cd8b91658aef092c1f
Riley Shahar <riley.shahar@gmail.com>
2022-08-31 17:07:13 -0700

    Bump patch version

--- src/aga/gradescope/resources/setup.py
+++ src/aga/gradescope/resources/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup  # type: ignore
 
 setup(
     name="aga",
-    version="0.8.4",
+    version="0.8.5",
     install_requires=[
         "dataclasses-json >= 0.5.6",
         "dill >= 0.3.4",
