18f473d433ef3ce5bbd12101e1e0807eb23c4e27
Larry Z <13360148+FlickerSoul@users.noreply.github.com>
2023-01-25 20:55:10 -0800

    chore: update gradescope setup to py3.11 to fix opcode 151 error

--- src/aga/gradescope/resources/setup.sh
+++ src/aga/gradescope/resources/setup.sh
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
-# install python 3.10
+# install python 3.11
 apt-get update -y
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
-apt-get install -y python3.10 python3.10-distutils
+apt-get install -y python3.11 python3.11-distutils
 
 # clean apt cache; this keeps the image small
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # install aga
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
 pip install -e /autograder/source
-python3.10 -m pip cache purge
+python3.11 -m pip cache purge
